5bda7edaf12f1344a3910c61642534d088f66dff
Danil <>
2025-07-21 20:33:59 +1000

    make project struct

---
+++ millionaireRush/millionaireRush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  millionaireRush
+//
+//  Created by Danil on 21.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
