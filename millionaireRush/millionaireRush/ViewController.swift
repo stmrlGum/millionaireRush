727a0592f233575b9170adc791fdcee92f2c2846
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-07-21 20:17:10 +0400

    добавил тест1

--- millionaireRush/millionaireRush/ViewController.swift
+++ millionaireRush/millionaireRush/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
-        // Do any additional setup after loading the view.
+        print("test1")
     }
 
 
