efcf883e6ccef74b928be609737d2198a6601c23
Alexander Ischenko <ischeenko@gmail.com>
2025-07-25 09:07:44 +0300

    Add GameVC

--- millionaireRush/millionaireRush/SceneDelegate.swift
+++ millionaireRush/millionaireRush/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewController = StartScreenVC()
-        viewController.hasSavedGame = true
+        let viewController = GameViewController()
+//        viewController.hasSavedGame = true
         window.rootViewController = viewController
         self.window = window
         window.makeKeyAndVisible()
